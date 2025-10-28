cd97273bcaa14fe7b471fab8e146d99cb3e2c2c7
gabriel@tsntx.com <gabriel@tsntx.com>
2025-10-28 15:20:55 -0500

    Add initial implementation of Graph App-Only authentication and user listing functionality

---
+++ graphhelper.go
@@ -0,0 +1,81 @@
+package graphhelper
+
+import (
+	"context"
+	"os"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	auth "github.com/microsoft/kiota-authentication-azure-go"
+	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
+	"github.com/microsoftgraph/msgraph-sdk-go/models"
+	"github.com/microsoftgraph/msgraph-sdk-go/users"
+)
+
+type GraphHelper struct {
+	clientSecretCredential *azidentity.ClientSecretCredential
+	appClient              *msgraphsdk.GraphServiceClient
+}
+
+func NewGraphHelpter() *GraphHelper {
+	g := &GraphHelper{}
+	return g
+}
+
+func (g *GraphHelper) InitializeGraphForAppOnlyAuth() error {
+	clientId := os.Getenv("CLIENT_ID")
+	tenantId := os.Getenv("TENANT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	credential, err := azidentity.NewClientSecretCredential(tenantId, clientId, clientSecret, nil)
+	if err != nil {
+		return err
+	}
+
+	g.clientSecretCredential = credential
+
+	authProvider, err := auth.NewAzureIdentityAuthenticationProviderWithScopes(g.clientSecretCredential, []string{"https://graph.microsoft.com/.default"})
+	if err != nil {
+		return err
+	}
+
+	adapter, err := msgraphsdk.NewGraphRequestAdapter(authProvider)
+	if err != nil {
+		return err
+	}
+
+	client := msgraphsdk.NewGraphServiceClient(adapter)
+	g.appClient = client
+
+	return nil
+}
+
+func (g *GraphHelper) GetAppToken() (*string, error) {
+	token, err := g.clientSecretCredential.GetToken(context.Background(), policy.TokenRequestOptions{
+		Scopes: []string{
+			"https://graph.microsoft.com/.default",
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &token.Token, nil
+}
+
+func (g *GraphHelper) GetUsers() (models.UserCollectionResponseable, error) {
+	var topValue int32 = 25
+	query := users.UsersRequestBuilderGetQueryParameters{
+		// Only request specific properties
+		Select: []string{"displayName", "id", "mail"},
+		// Get at most 25 results
+		Top: &topValue,
+		// Sort by display name
+		Orderby: []string{"displayName"},
+	}
+
+	return g.appClient.Users().
+		Get(context.Background(),
+			&users.UsersRequestBuilderGetRequestConfiguration{
+				QueryParameters: &query,
+			})
+}
